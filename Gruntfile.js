ca952593158f40698808c46a3bd722073c3d5e14
Fabio Bier <fab.bier@gmail.com>
2017-10-23 00:13:02 +0200

    Restructured the project and added Grunt as a task runner and builder.

---
+++ Gruntfile.js
@@ -0,0 +1,145 @@
+module.exports = function(grunt) {
+
+    require('load-grunt-tasks')(grunt);
+    
+    // Configurable paths
+    var config = {
+        testPath: 'test',
+        distPath: 'dist',
+        tmpPath: '.tmp',
+        appRoot: 'src',
+        staticRoot: 'src',
+        sassPath: 'scss',
+        cssPath: 'css',
+        jsPath: 'js',
+        imgPath: 'img'
+    };
+    
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+        // Project settings
+        config: config,
+        
+        csscomb: {
+            default: {
+                options: {
+                    config: 'csscomb.json'
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%= config.staticRoot %>/<%= config.sassPath %>/',
+                    src: ['**/*.scss'],
+                    dest: '<%= config.staticRoot %>/<%= config.sassPath %>/',
+                    ext: '.scss'
+                }]
+            }
+        },
+
+		sass: {
+			dist: {
+				files: {
+					'src/css/main.css' : 'src/<%= config.sassPath %>/main.scss'
+				}
+			}
+		},
+
+        concat: {
+            css: {
+                src: [
+                    'src/css/*'
+                ],
+                dest: '<%= config.tmpPath %>/css/main.css'
+            },
+            js: {
+                options: {
+                    separator: ';\n',
+                },
+                src: [
+                    'node_modules/jquery/dist/jquery.min.js',
+                    'src/js/analytics-experiments.js'
+                ],
+                dest: '<%= config.tmpPath %>/js/main.js'
+            }
+        },
+
+        cssmin: {
+            css: {
+                src: '<%= config.tmpPath %>/css/main.css',
+                dest: '<%= config.distPath %>/css/main.css'
+            }
+        },
+
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'dist/index.html': 'src/index.html',
+                    'dist/404.html': 'src/404.html'
+                }
+            }
+        },
+
+        //compressing images
+        imagemin: {
+            dist: {
+                options: {
+                    optimizationLevel: 3
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%= config.appRoot %>/img',
+                    src: ['**/*.{png,jpg,gif,webp}'],
+                    dest: '<%= config.distPath %>/img'
+                }]
+            }
+        },
+
+        uglify: {
+            js: {
+                files: {
+                    '<%= config.distPath %>/js/main.js': ['<%= config.tmpPath %>/js/main.js']
+                }
+            }
+        },
+
+        // Combine all media queries
+	    cmq: {
+		    options: {
+			    log: false
+			},
+			target: {
+				files: {
+					'src/css/main.css': ['src/css/main.css']
+				}
+			}
+		},
+
+        watch: {
+            css: {
+                files: '**/*.scss',
+                tasks: ['csscomb', 'sass', 'cmq', 'concat:css', 'cssmin:css', 'concat:js', 'uglify:js']
+            }
+        },
+
+        clean: {
+            folder: ['<%= config.tmpPath %>']
+        }
+	});
+
+    grunt.registerTask('default', [
+        'csscomb',
+        'sass',
+        'cmq',
+        'concat:css',
+        'cssmin:css',
+        'concat:js',
+        'imagemin',
+        'uglify:js',
+        'htmlmin',
+        'clean'
+    ]);
+}
---
+++ src/js/analytics-experiments.js
@@ -0,0 +1,59 @@
+var experiments = {
+    "-5jgBCpwTBGL2Rl44bDygw": [
+        function() {
+            console.log('Control'),
+                ga("set", "dimensionX", "GTM-001_Control"),
+                ga("send", "event", "AB-test", "GTM-001 Name of the test", "Control", {nonInteraction: 1});
+        },
+        function() {
+            document.getElementById('optin').checked = true;
+            console.log('Variation 1'),
+                ga("set", "dimensionX", "GTM-001_Variation_1"),
+                ga("send", "event", "AB-test", "GTM-001 Name of the test", "Variation 1", {nonInteraction: 1});
+        }
+    ]
+};
+
+function getExperiment() {
+    var acksinExp = localStorage.getItem("acksinExperiments");
+
+    // Get a new experiment
+    if(acksinExp == null) {
+        var expKeys = Object.keys(experiments);
+        acksinExp = expKeys[Math.floor(Math.random() * expKeys.length)];
+
+        localStorage.setItem("acksinExperiments", acksinExp);
+        return acksinExp;
+    }
+
+    // Check if experiment is still running, if it is get it.
+    if(acksinExp in experiments) {
+        return acksinExp;
+    }
+
+    // Remove the existing experiment and recurse so we can get the new experiment.
+    localStorage.removeItem("acksinExperiments");
+    return getExperiment();
+}
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+<!-- Force variation by URL argument -->
+var varID = getParameterByName('var');
+var chosenVariation = null;
+
+if (!varID) {
+    chosenVariation = cxApi.chooseVariation();
+}
+else {
+    chosenVariation = varID;
+}
+
+$.getScript("https://www.google-analytics.com/cx/api.js?experiment=" + getExperiment(), function(){
+    experiments[getExperiment()][chosenVariation]();
+});
